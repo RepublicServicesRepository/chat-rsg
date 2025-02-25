b3b099bace98e4de2b9bf68384283a2a4b286267
Kolasani <hkolasani@repsrv.com>
2025-02-25 06:45:37 +0530

    prod readiness

--- backend/app/agents/utils.py
+++ backend/app/agents/utils.py
@@ -1,16 +1,16 @@
 from app.agents.tools.base import BaseTool
-from app.agents.tools.rsg.ask_kmt.tool import ask_kmt_tool
+# from app.agents.tools.rsg.ask_kmt.tool import ask_kmt_tool
 from app.agents.tools.rsg.ask_sop.tool import ask_sop_tool
-from app.agents.tools.rsg.mpu.tool import mpu_tool
-from app.agents.tools.rsg.ask_jeff.tool import ask_jeff_tool
+# from app.agents.tools.rsg.mpu.tool import mpu_tool
+# from app.agents.tools.rsg.ask_jeff.tool import ask_jeff_tool
 
 
 def get_available_tools() -> list[BaseTool]:
     tools: list[BaseTool] = []
-    tools.append(ask_kmt_tool)
+    #tools.append(ask_kmt_tool)
     tools.append(ask_sop_tool)
-    tools.append(mpu_tool) 
-    tools.append(ask_jeff_tool)
+    #tools.append(mpu_tool) 
+    #tools.append(ask_jeff_tool)
     return tools
 
 
